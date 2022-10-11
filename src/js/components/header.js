3399d5fbdbdbb036c66ab21de31001c1c39e3431
AlexGlukhov-dev <alexglukhov37@gmail.com>
2022-10-11 16:18:25 +0300

    fix 11_10_22

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,5 +1,6 @@
 const progress = document.getElementById("progressbar");
 const rotated = document.getElementById("menu-button");
+const upBtn = document.querySelector(".btn-up");
 
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 // progress.style.height = '0';
@@ -25,4 +26,14 @@ window.addEventListener("scroll", () => {
   let offset = window.scrollY * 0.2;
 
   rotated.style.transform = `rotate(${offset}deg)`;
+
+  if (window.scrollY > 100) {
+    upBtn.classList.remove("visually-hidden")
+    upBtn.style.opacity = 1;
+  } else {
+    upBtn.classList.add("visually-hidden")
+    upBtn.style.opacity = 0;
+  }
 });
+
+
