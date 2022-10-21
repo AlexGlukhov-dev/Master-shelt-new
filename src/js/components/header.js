1264a802e22501cf269a86302ece6f2a91733e6f
Vladinero <vlad.servicemail@gmail.com>
2022-10-21 15:27:27 +0300

    Changed parallax speed hero section

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -14,8 +14,8 @@ const parallax = (e) => {
 document.querySelectorAll('.layer')
   .forEach(layer => {
     const speed = layer.getAttribute('data-speed')
-    const x = (window.innerWidth - e.pageX * speed) / 100
-    const y = (window.innerHeight - e.pageY * speed) / 100
+    const x = (window.innerWidth - e.pageX * speed) / 50
+    const y = (window.innerHeight - e.pageY * speed) / 50
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`
   })
 }
